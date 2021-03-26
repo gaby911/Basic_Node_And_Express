98298eeac96e2fe94dbc39dfc53dbcbccbe2f3b6
Florin Gabriel Abagiu <35663669+gaby911@users.noreply.github.com>
2021-03-26 14:44:41 +0200

    adding middleware

--- myApp.js
+++ myApp.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
 
-app.use("/public",express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use(function (req, res, next) {
+    // Do something
+    var string = req.method + " " + req.path + " - " + req.ip;
+    console.log(string);
+    // Call the next function in line:
+    next();
+  });
 
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
@@ -17,7 +25,28 @@ app.get("/json", function(req, res) {
     }
   });
 
+  app.get("/now", 
+  function(req, res, next){
+      req.time = new Date().toString();
+        next();
+  },function(req,res){
+      res.send({
+          time: req.time
+      });
+    }
+  );
 
+ /* const middleware = (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+  };
+  
+  app.get("/now", middleware, (req, res) => {
+    res.send({
+      time: req.time
+    });
+  });*/
+  
 
 
 
