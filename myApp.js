888a9b5006fdf257354b20f09e7da83d5c6ba6f1
Florin Gabriel Abagiu <35663669+gaby911@users.noreply.github.com>
2021-03-26 13:01:29 +0200

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -1,12 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.use("/public",express.static(__dirname + "/public"));
+
 app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
   });
 
 
 
+app.get("/json", function(req, res) {
+    res.json({"message": "Hello json"});
+  });
 
 
 
