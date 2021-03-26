1f4fa9dac2b667ab6c087db001e20804a5c5e69f
Florin Gabriel Abagiu <35663669+gaby911@users.noreply.github.com>
2021-03-26 12:48:19 +0200

    add relative path to express

--- myApp.js
+++ myApp.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
 
-app.get("/", function(req,res){
-    res.send("Hello Express");
-});
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 
 
